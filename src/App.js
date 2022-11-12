90c38b64b2dce689f6d45ea7988d1d782644dfb7
Frederic Guilloux <frederic.guilloux85@gmail.com>
2022-11-12 16:05:06 +1300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
+import { ThemeContext } from './context/ThemeContext';
 import Header from './header/Header';
 import About from './about/About';
 import Projects from './projects/Projects';
 import Timeline from './timeline/Timeline';
 import Contact from './contact/Contact';
 import { useContext } from 'react';
-import { ThemeContext } from './context/ThemeContext';
+import Footer from './about/footer/Footer';
 
 function App() {
   const { darkTheme, handleClick } = useContext(ThemeContext);
@@ -19,6 +20,7 @@ function App() {
       <Projects darkTheme={darkTheme} />
       <Timeline darkTheme={darkTheme} />
       <Contact darkTheme={darkTheme} />
+      <Footer darkTheme={darkTheme} />
     </div>
   );
 }
